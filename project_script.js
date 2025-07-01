ca635ab36fce7b0c0f1568050c8bac074d196ecf
ud123df <147704288+ud123df@users.noreply.github.com>
2025-07-01 17:37:36 +0530

    Add files via upload

---
+++ project_script.js
@@ -0,0 +1,13 @@
+alert('welcome to OURTES');
+let a=('i am a coder and the best full stck developer');
+const b = 1500;
+console.log(a, b);
+//select the element with class 
+const headerTitle= document.querySelector('.header-title');
+
+
+//responding user action 
+const colorBtn = document.getElementById('colorBtn');
+colorBtn.addEventListener('click', function() {
+    headerTitle.computedStyleMap.color = '#ffd700';
+})
