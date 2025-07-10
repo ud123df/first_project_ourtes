8373a6eb3f2987fe10c50f5e36d061057495a5d9
ud123df <147704288+ud123df@users.noreply.github.com>
2025-07-10 11:31:54 +0530

    Update project_script.js

--- project_script.js
+++ project_script.js
@@ -2,12 +2,12 @@ alert('welcome to OURTES');
 let a=('i am a coder and the best full stck developer');
 const b = 1500;
 console.log(a, b);
-//select the element with class 
+//select the element with class
 const headerTitle= document.querySelector('.header-title');
 
 
-//responding user action 
+//responding user action
 const colorBtn = document.getElementById('colorBtn');
 colorBtn.addEventListener('click', function() {
-    headerTitle.computedStyleMap.color = '#ffd700';
+    headerTitle.style.color = '#ffd700'; // Corrected line: use .style instead of .computedStyleMap
 })
